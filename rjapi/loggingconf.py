a965ee478af3890955f3c3e25f402b987a898a9b
Nicholas Chai <nicholas.chai@intelligis.io>
2025-02-17 16:52:11 +0800

    - upgrade to json logging for files - added uuid to logs for request isolation - added execption logging hanlder - add logging to account routes as examples - added rich console logging formatter

--- rjapi/loggingconf.py
+++ rjapi/loggingconf.py
@@ -8,11 +8,23 @@ def configure_logging() -> None:
         {
             "version": 1,
             "disable_existing_loggers": False,
+            "filters": {
+                "correlation_id": {
+                    "()": "asgi_correlation_id.CorrelationIdFilter",
+                    "uuid_length": 8 if isinstance(config, DevConfig) else 32,
+                    "default_value": "-",
+                }
+            },
             "formatters": {
                 "console": {
                     "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "(%(correlation_id)s) %(name)s:%(lineno)d - %(message)s",
+                },
+                "file": {
+                    "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
                     "datefmt": "%Y-%m-%d %H:%M:%S",
-                    "format": "%(name)s:%(lineno)d - %(message)s",
+                    "format": "%(asctime)s.%(msecs)03d %(levelname)-8s %(correlation_id)s %(name)s%(lineno)d %(message)s",
                 },
             },
             "handlers": {
@@ -20,13 +32,33 @@ def configure_logging() -> None:
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
+                    "filters": ["correlation_id"],
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "rjapi.log",
+                    "maxBytes": 1024 * 1024 * 3,  # 3MB
+                    "backupCount": 3,
+                    "encoding": "utf-8",
+                    "filters": ["correlation_id"],
                 },
             },
             "loggers": {
+                "uvicorn": {"handlers": ["default"], "level": "INFO"},
                 "rjapi": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     "propagate": False,
+                },
+                "databases": {
+                    "handlers": ["default"],
+                    "level": "WARNING",
+                },
+                "aiosqlite": {
+                    "handlers": ["default"],
+                    "level": "WARNING",
                 },
             },
         }
--- rjapi/main.py
+++ rjapi/main.py
@@ -1,7 +1,9 @@
 import logging
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from asgi_correlation_id import CorrelationIdMiddleware
+from fastapi import FastAPI, HTTPException
+from fastapi.exception_handlers import http_exception_handler
 
 from rjapi.database import database
 from rjapi.loggingconf import configure_logging
@@ -20,6 +22,12 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
+app.add_middleware(CorrelationIdMiddleware)
 
 app.include_router(post_router)
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler_logging(request, exc):
+    logger.error(f"HTTPException: {exc.status_code} - {exc.detail}")
+    return await http_exception_handler(request, exc)
